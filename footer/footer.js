bea913a416372df51c50d7f5d8343fa829cf7037
jefuentes <jossuerm55@gmail.com>
2024-05-30 12:13:41 -0600

    se completa el footer

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,12 @@
 let footer_DOM = document.querySelector(".footer");
 
-footer_DOM.innerHTML = `Hola soy footer`;
+footer_DOM.innerHTML = `
+
+<div class="div-colaboradores">
+    <a href="https://github.com/ROCKEMMA">@ROCKEMMA</a>
+    <a href="https://github.com/aamelendez">@aamelendez</a>
+    <a href="https://github.com/dalunaaaa">@dalunaaaa</a>
+    <a href="https://github.com/mclovin65">@mclovin65</a>
+</div>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMaMg2Z9Y0pBgBvfseJaKn2Re3yRSdGo9182O8DLPpWN5OFS-DyIhdR0LSOQolXR4hADk&usqp=CAU" alt="logo scl">
+`;
