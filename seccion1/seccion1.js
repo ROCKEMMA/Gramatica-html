ad6c0a016a4766ee49827fae5ddfb70a03fbe8be
danialuna <dmluna@scl.edu.gt>
2024-05-30 12:09:55 -0600

    seccion1-Dania

--- seccion1/seccion1.js
+++ seccion1/seccion1.js
@@ -1,3 +1,33 @@
 let seccion1_DOM = document.querySelector(".seccion1");
 
-seccion1_DOM.innerHTML = `Hola soy sección 1`;
+seccion1_DOM.innerHTML = `
+<h2>Bloque</h2>
+<table>
+  <tr>
+    <th>Nombre</th>
+    <th>Descripción</th>
+  </tr>
+  <tr>
+    <td>  &lt;html&gt; &lt;/html&gt; </td>
+    <td>Lenguaje de marcado de hipertexto.</td>
+  </tr>
+  <tr>
+  <tr>
+  <td>&lt;head&gt;&lt;/head&gt;&lt;&gt; </td>
+  <td>Sección de encabezado del documento.</td>
+</tr>
+<tr>
+  <td>&lt;body&gt;&lt;/body&gt;&lt;&gt; </td>
+  <td>Contenido principal del documento.</td>
+</tr>
+<tr>
+  <td>&lt;p&gt;&lt;/p&gt;&lt;&gt; </td>
+  <td>Párrafo.</td>
+</tr>
+<tr>
+  <td>&lt;a href=""&gt;&lt;/a&gt;&lt;&gt; </td>
+  <td>Enlace.</td>
+</tr>
+ 
+</table>
+  `;
